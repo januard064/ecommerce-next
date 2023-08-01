bc2e92fdaa8d26c46b0736f0974c5978611b92ed
Januard Lumbangaol <januard.lumbangaol@commsult.id>
2023-08-01 22:34:07 +0700

    provide carousel frame

---
+++ components/carousel/carousel.js
@@ -0,0 +1,9 @@
+const Carousel = () => {
+    return (
+        <div class="h-36 w-full bg-[#212844]">
+            <div>Carousel Title</div>
+        </div>
+    )
+}
+
+export default Carousel
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import ProductCardList from '@/components/product-card/product-card-list'
+import Carousel from '@/components/carousel/carousel'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <div style={{ color: 'red', fontFamily:'Poppins-Bold', fontSize:'48px'  }}>Landing Page</div>
+      <Carousel />
       <ProductCardList />
     </>
   )
