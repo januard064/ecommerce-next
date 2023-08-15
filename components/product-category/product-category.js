d6928bc9a2466dcc2ceb6b0daa412d6107ab8e2d
Januard Lumbangaol <januard.lumbangaol@commsult.id>
2023-08-15 10:56:00 +0700

    provide custom images for category products

--- components/product-category/product-category.js
+++ components/product-category/product-category.js
@@ -7,7 +7,7 @@ const ProductCategory = (props) => {
     return (
         <div className="w-[132px]">
             <div className="h-[132px] w-[132px] bg-[#F5F5F5] rounded-full flex justify-center items-center hover:border-[#008ECC] cursor-pointer border hover:shadow-[0_25px_50px_-12px_rgba(0,0,0,0.25)]">
-                <Image src={`/images/category.png`} width={52} height={98} />
+                <Image src={`/images/category-images/groceries.png`} width={82} height={120} />
             </div>
             <div className="flex justify-center mt-[20px]">
                 <p style={{ fontFamily: 'Poppins-Regular' }} className="text-[#222222] text-sm">{category.title}</p>
